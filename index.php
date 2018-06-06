6a70f50ef36e3c5b2616f16e549467bd269527e1
Geoffrey Stephenson <geoffrey@industryfive.com>
2018-06-05 22:06:50 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+$ch = curl_init($image_path);
+curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_exec($ch);
+$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+curl_close($ch);
+
+// $retcode = 200 = found
+// $retcode > 400 = not found
